295afe25d3c4dbe642b1befca2647934d99f5b46
Anowar Hossain <anowar3042@gmail.com>
2022-08-25 10:49:29 +0530

    add

--- src/Redux/AuthReducer/actionTypes.jsx
+++ src/Redux/AuthReducer/actionTypes.jsx
@@ -1,4 +1,4 @@
 export const GET_LOGIN_REQUEST = "GET_LOGIN_REQUEST";
 
-export const GET_LOGIN = "GET_LOGIN";
+export const GET_LOGIN_SUCCESS = "GET_LOGIN_SUCCESS";
 export const GET_LOGIN_FAILURE = "GET_LOGIN_FAILURE";
