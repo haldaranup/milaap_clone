b9f76e764025525a654439a38acc96c4fad86ff3
Altaf Ali Baig <altaf100001@gmail.com>
2022-08-28 00:25:44 +0530

    homepage_updated_final

--- src/Redux/AppReducer/reducer.js
+++ src/Redux/AppReducer/reducer.js
@@ -3,10 +3,10 @@ import * as types from "./actionTypes";
 const initialState = {
   donate: [],
   lendData: [],
+  homeData: [],
   isLoading: false,
   isError: false,
 };
-
 export const reducer = (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
@@ -43,6 +43,26 @@ export const reducer = (state = initialState, action) => {
         isError: false,
       };
     case types.GET_LEND_DATA_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    ///
+    case types.GET_HOME_DATA_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case types.GET_HOME_DATA_SUCCESS:
+      return {
+        ...state,
+        homeData: payload,
+        isLoading: false,
+        isError: false,
+      };
+    case types.GET_HOME_DATA_FAILURE:
       return {
         ...state,
         isLoading: false,
